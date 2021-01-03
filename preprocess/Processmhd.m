80b79f1ad36563c10fd2121c6d12cd731a297e17
DeepFLASH2020 <jw4hv@virginia.edu>
2021-01-03 17:49:41 -0500

    change the file format of preprocessing

--- preprocess/Processmhd.m
+++ preprocess/Processmhd.m
@@ -31,19 +31,19 @@ for k=1:size(FilesV)
     if (lth ~= 1)
     alldataVf(k,:,:,:,:)= velocity2fourier3D(reshape(alldataV(k,:,:,:,:),comdim,hth,wth,lth));
     alldataVf(k,:,:,:,:) = alldataVf(k,:,:,:,:)/(hth*wth*lth);
-    allLowVf (k,:,:,:,:) = TruncatedLowF(alldataVf(k,:,:,:,:),trunc_dim,trunc_dim,trunc_dim);
-    fname_vel_R_X = sprintf('./velo_fourier_real_x/%dvelocityRFX.mhd', k);
+    allLowVf (k,:,:,:,:) = TruncatedLowF3D(alldataVf(k,:,:,:,:),trunc_dim,trunc_dim,trunc_dim);
+    fname_vel_R_X = sprintf('./Rnet/velo_fourier_real_x/%dvelocityRFX.mhd', k);
     writeMETA(reshape(real(allLowVf(k,1,:,:,:)), trunc_dim+1, trunc_dim+1, trunc_dim+1),fname_vel_R_X);
-    fname_vel_R_Y = sprintf('./velo_fourier_real_y/%dvelocityRFY.mhd', k);
+    fname_vel_R_Y = sprintf('./Rnet/velo_fourier_real_y/%dvelocityRFY.mhd', k);
     writeMETA(reshape(real(allLowVf(k,2,:,:,:)), trunc_dim+1, trunc_dim+1, trunc_dim+1),fname_vel_R_Y);
-    fname_vel_R_Z = sprintf('./velo_fourier_real_z/%dvelocityRFZ.mhd', k);
+    fname_vel_R_Z = sprintf('./Rnet/velo_fourier_real_z/%dvelocityRFZ.mhd', k);
     writeMETA(reshape(real(allLowVf(k,3,:,:,:)), trunc_dim+1, trunc_dim+1, trunc_dim+1),fname_vel_R_Z);
 
-    fname_vel_R_X = sprintf('./velo_fourier_imag_x/%dvelocityIFX.mhd', k);
+    fname_vel_R_X = sprintf('./Inet/velo_fourier_imag_x/%dvelocityIFX.mhd', k);
     writeMETA(reshape(imag(allLowVf(k,1,:,:,:)), trunc_dim+1, trunc_dim+1, trunc_dim+1),fname_vel_R_X);
-    fname_vel_R_Y = sprintf('./velo_fourier_imag_y/%dvelocityIFY.mhd', k);
+    fname_vel_R_Y = sprintf('./Inet/velo_fourier_imag_y/%dvelocityIFY.mhd', k);
     writeMETA(reshape(imag(allLowVf(k,2,:,:,:)), trunc_dim+1, trunc_dim+1, trunc_dim+1),fname_vel_R_Y);
-    fname_vel_R_Z = sprintf('./velo_fourier_imag_z/%dvelocityIFZ.mhd', k);
+    fname_vel_R_Z = sprintf('./Inet/velo_fourier_imag_z/%dvelocityIFZ.mhd', k);
     writeMETA(reshape(imag(allLowVf(k,3,:,:,:)), trunc_dim+1, trunc_dim+1, trunc_dim+1),fname_vel_R_Z);
 
     %%%%%%%%%%%%%%%%%%%%%%%%%% Extracted Low-frequencies of 2D velocity fields %%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -52,18 +52,18 @@ for k=1:size(FilesV)
     alldataVf(k,:,:,:) = alldataVf(k,:,:,:)/(hth*wth*lth);
     allLowVf (k,:,:,:) = TruncatedLowF(alldataVf(k,:,:,:),trunc_dim,trunc_dim,1);
 
-    fname_vel_R_X = sprintf('./velo_fourier_real_x/%dvelocityRFX.mhd', k);
+    fname_vel_R_X = sprintf('./Rnet/velo_fourier_real_x/%dvelocityRFX.mhd', k);
     writeMETA(reshape(real(allLowVf(k,1,:,:)), trunc_dim+1, trunc_dim+1),fname_vel_R_X);
-    fname_vel_R_Y = sprintf('./velo_fourier_real_y/%dvelocityRFY.mhd', k);
+    fname_vel_R_Y = sprintf('./Rnet/velo_fourier_real_y/%dvelocityRFY.mhd', k);
     writeMETA(reshape(real(allLowVf(k,2,:,:)), trunc_dim+1, trunc_dim+1),fname_vel_R_Y);
-    fname_vel_R_Z = sprintf('./velo_fourier_real_z/%dvelocityRFZ.mhd', k);
+    fname_vel_R_Z = sprintf('./Rnet/velo_fourier_real_z/%dvelocityRFZ.mhd', k);
     writeMETA(reshape(real(allLowVf(k,3,:,:)), trunc_dim+1, trunc_dim+1),fname_vel_R_Z);
 
-    fname_vel_R_X = sprintf('./velo_fourier_imag_x/%dvelocityIFX.mhd', k);
+    fname_vel_R_X = sprintf('./Inet/velo_fourier_imag_x/%dvelocityIFX.mhd', k);
     writeMETA(reshape(imag(allLowVf(k,1,:,:)), trunc_dim+1, trunc_dim+1),fname_vel_R_X);
-    fname_vel_R_Y = sprintf('./velo_fourier_imag_y/%dvelocityIFY.mhd', k);
+    fname_vel_R_Y = sprintf('./Inet/velo_fourier_imag_y/%dvelocityIFY.mhd', k);
     writeMETA(reshape(imag(allLowVf(k,2,:,:)), trunc_dim+1, trunc_dim+1),fname_vel_R_Y);
-    fname_vel_R_Z = sprintf('./velo_fourier_imag_z/%dvelocityIFZ.mhd', k);
+    fname_vel_R_Z = sprintf('./Inet/velo_fourier_imag_z/%dvelocityIFZ.mhd', k);
     writeMETA(reshape(imag(allLowVf(k,3,:,:)), trunc_dim+1, trunc_dim+1),fname_vel_R_Z);
     end
 
@@ -76,9 +76,9 @@ for k=1:size(FilesV)
     if (lth ~= 1)
     alldataSf(k,:,:,:)= image2fourier3D(reshape(alldataS(k,:,:,:,:),hth,wth,lth));
     alldataSf(k,:,:,:) = alldataSf(k,:,:,:)/ (hth*wth*lth);
-    fname_src_R = sprintf('./src_fourier_real/%dsrcRF.mhd', k);
+    fname_src_R = sprintf('./Rnet/src_fourier_real/%dsrcRF.mhd', k);
     writeMETA(reshape(real(alldataSf(k,:,:,:)),hth, wth, lth),fname_src_R);  
-    fname_src_I = sprintf('./SourceFourierImag/%dsrcIF.mhd', k);
+    fname_src_I = sprintf('./Inet/src_fourier_imag/%dsrcIF.mhd', k);
     writeMETA(reshape(imag(alldataSf(k,:,:,:)),hth, wth, lth),fname_src_I);
     % alldataSfcon(k,:,:)= reshape(getconj(reshape(alldataSf(k,:,:),hth,wth)),1,floor(hth/2)+1,wth);
     else
@@ -86,7 +86,7 @@ for k=1:size(FilesV)
     alldataSf(k,:,:) = alldataSf(k,:,:)/ (hth*wth);
     fname_src_R = sprintf('./src_fourier_real/%dsrcRF.mhd', k);
     writeMETA(reshape(real(alldataSf(k,:,:,:)),hth, wth ),fname_src_R);  
-    fname_src_I = sprintf('./SourceFourierImag/%dsrcIF.mhd', k);
+    fname_src_I = sprintf('./src_fourier_imag/%dsrcIF.mhd', k);
     writeMETA(reshape(imag(alldataSf(k,:,:,:)),hth, wth ),fname_src_I);  
     end
     
@@ -98,17 +98,17 @@ for k=1:size(FilesV)
     if (lth ~= 1)
     alldataTf(k,:,:,:)= image2fourier(reshape(alldataT(k,:,:,:),hth,wth,lth));
     alldataTf(k,:,:,:) = alldataTf(k,:,:,:)/ (hth*wth*lth);
-    fname_tar_R = sprintf('./tar_fourier_real/%dtarRF.mhd', k);
+    fname_tar_R = sprintf('./Rnet/tar_fourier_real/%dtarRF.mhd', k);
     writeMETA(reshape(real(alldataTf(k,:,:,:)), hth, wth, lth),fname_tar_R);
-    fname_tar_I = sprintf('./TargetFourierImag/%dtarIF.mhd', k);
+    fname_tar_I = sprintf('./Inet/tar_fourier_imag/%dtarIF.mhd', k);
     writeMETA(reshape(imag(alldataTf(k,:,:,:)), hth, wth, lth),fname_tar_I);
     % alldataTfcon(k,:,:)= reshape(getconj(reshape(alldataSf(k,:,:),hth,wth)),1,floor(hth/2)+1,wth);  
     else
     alldataTf(k,:,:)= image2fourier(reshape(alldataT(k,:,:,:),hth,wth));
     alldataTf(k,:,:) = alldataTf(k,:,:)/ (hth*wth);
-    fname_tar_R = sprintf('./tar_fourier_real/%dtarRF.mhd', k);
+    fname_tar_R = sprintf('./Rnet/tar_fourier_real/%dtarRF.mhd', k);
     writeMETA(reshape(real(alldataTf(k,:,:)), hth, wth),fname_tar_R);
-    fname_tar_I = sprintf('./TargetFourierImag/%dtarIF.mhd', k);
+    fname_tar_I = sprintf('./Inet/tar_fourier_imag/%dtarIF.mhd', k);
     writeMETA(reshape(imag(alldataTf(k,:,:)), hth, wth),fname_tar_I);
     end
 end
--- preprocess/TruncatedLowF3D.m
+++ preprocess/TruncatedLowF3D.m
@@ -5,11 +5,13 @@ function y = TruncatedLowF3D (x, truncX, truncY, truncZ)
 x=reshape(x,cop,h,w,d);
 %%%%%%%%%%%%%%%%%%%Cut high frequency %%%%%%%%%%%%%%%% 
 y = zeros(cop, truncX+1, truncY+1, truncZ+1);
+
 for id = 1:cop
 for i = ((h/2)- (truncX/2)+ 1): ((h/2)- (truncX/2)+ truncX+1)
     for j =((w/2)- (truncY/2) +1 ): ((w/2)- (truncY/2) + truncY+1 )
-        for k =((d/2)- (truncZ/2 +1 )): ((h/2) + (truncZ/2) + truncZ+1 )
-         y (id, i-(h/2-truncX/2), j-(w/2-truncY/2), k - (d/2-truncZ/2)) = x (id,i,j,k);
+        for k =((d/2)- (truncZ/2) +1 ): ((d/2) - (truncZ/2) + truncZ+1 )
+           
+         y (id, i-(h/2-truncX/2), j-(w/2-truncY/2), k-(d/2-truncZ/2)) = x (id,i,j,k);
         end
     end
 end
